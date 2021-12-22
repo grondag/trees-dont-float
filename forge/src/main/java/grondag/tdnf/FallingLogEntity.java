1f2f8d6cd3605798d7ecf58a001ceae264a9da24
grondag <grondag@users.noreply.github.com>
2021-12-22 08:52:07 -0800

    Fix cstyle issues in Forge source

--- forge/src/main/java/grondag/tdnf/FallingLogEntity.java
+++ forge/src/main/java/grondag/tdnf/FallingLogEntity.java
@@ -20,33 +20,13 @@
 
 package grondag.tdnf;
 
-import io.netty.buffer.Unpooled;
 import net.minecraftforge.fmllegacy.common.registry.IEntityAdditionalSpawnData;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.item.FallingBlockEntity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.DirectionalPlaceContext;
-import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SupportType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.Shapes;
 
-import grondag.tdnf.Platform;
-import grondag.tdnf.TreesDoNotFloat;
-import grondag.tdnf.config.Configurator;
 import grondag.tdnf.world.BaseFallingLogEntity;
 
 public class FallingLogEntity extends BaseFallingLogEntity implements IEntityAdditionalSpawnData {
